bb11c3f0ca2a96b2309028245d6ed1685ed71bb5
Elliott Kember <elliott.kember@gmail.com>
2020-04-11 20:14:51 -0700

    Better setup

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 ### Soulmate
 
-## 1. Installation:
+## 1. Clone this repo
+
+```
+git clone git@github.com:Soulmate-Lights/soulmate-example.git
+cd soulmate-example
+```
+
+## 2. Installation:
 ```
 ./install
 ```
 
-## 2. Customize your light:
+## 3. Customize your code:
 Edit `main/main.cpp`.
 
-## 3. Compile and upload:
+## 4. Plug in your ESP32
+Your LEDs should be wired like so:
+
+- Ground -> Ground
+- DATA -> pin 18
+- CLOCK -> pin 23 (if using APA102)
+- 5V -> 5V
+
+## 4. Compile and upload:
 ```
 ./run
 ```
--- main/main.cpp
+++ main/main.cpp
@@ -1,25 +1,22 @@
 // Don't forget to change this!
 #define FIRMWARE_NAME "soulmate-example"
-
 // The number of LEDs in each parallel strip
 #define LED_COLS 32
 // The number of parallel strips
 #define LED_ROWS 2
 // Normally LED_COLS * LED_ROWS
 #define N_LEDS 64
-
+// If you're using WS2812B LED strips, uncomment this line
+// #define USE_WS2812B true
 // How long should we spend in each pattern?
 #define CYCLE_LENGTH_IN_MS 120000
-
 // How long should the Soulmate fade between patterns?
 #define FADE_DURATION 3000
-
 // Total power in milliamps
 #define SOULMATE_MILLIAMPS 700
-
 #include <Soulmate.h>
 
-// Write your pattern functions like this:
+// Write your pattern functions below:
 void warm() {
   fill_solid(Soulmate.led_arr, N_LEDS, CRGB::Peru);
 }
@@ -36,7 +33,6 @@ void purple() {
   fill_solid(Soulmate.led_arr, N_LEDS, CRGB::Purple);
 }
 
-// Or more complicated ones like this:
 float rainbowHue = 0;
 void rainbow() {
   rainbowHue += beatsin16Float(2, 0.01, 0.5);
@@ -72,7 +68,7 @@ void setup() {
   Soulmate.setup();
 }
 
-// Leave this the way it is!
+// Leave this function the way it is!
 void loop() {
   Soulmate.loop();
 }
